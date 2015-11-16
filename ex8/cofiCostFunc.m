9aea9a422aaf88bc395b7bfcbbb1e94e0f818eb3
Olek Janiszewski <olek.janiszewski@gmail.com>
2015-11-16 08:53:32 +0100

    Implement cost function and gradient

--- ex8/cofiCostFunc.m
+++ ex8/cofiCostFunc.m
@@ -40,9 +40,9 @@ Theta_grad = zeros(size(Theta));
 %                     partial derivatives w.r.t. to each element of Theta
 %
 
-
-
-
+J = 0.5 * sum(sum(R .* (X * Theta' - Y) .^ 2)) + 0.5 * lambda * sum(sum(Theta .^ 2)) + 0.5 * lambda * sum(sum(X .^ 2));
+X_grad = R .* (X * Theta' - Y) * Theta + lambda * X;
+Theta_grad = R' .* (X * Theta' - Y)' * X + lambda * Theta;
 
 
 
